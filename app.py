892020344739ea323ef60dcd726cc05f6f3839e5
Ivan Art <tobedrive@yandex.ru>
2024-07-06 09:00:00 +0300

    Добавил HTML разметку и Политику конфиденциальности

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import asyncio
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
+from aiogram.enums import ParseMode
+from aiogram.client.bot import DefaultBotProperties
 
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
@@ -10,7 +12,7 @@ load_dotenv(find_dotenv())
 from main_folder.handlers import handlers_router
 
 
-bot = Bot(token=os.getenv("TOKEN"))
+bot = Bot(token=os.getenv("TOKEN"), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
 dp.include_router(handlers_router)
